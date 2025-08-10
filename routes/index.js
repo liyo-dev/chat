8bab97daeefe32fc222cd4c3fb9ae10fb25aeac9
Raúl Báez <luarbaz@gmail.com>
2025-08-10 23:50:29 +0200

    Versión Vercel: demo estático sin Socket.io, diseño retro completo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index-vercel', { title: 'Chat LIYODEV' });
 });
 
 module.exports = router;
